1d07dffc712239d756bc87dd46210b45419415d0
Al Hodges <walter_hodges@mindtree.com>
2014-02-24 14:11:20 -0500

    Initial checkin of NUnitSamples

---
+++ NUnitSamples/NUnitSample1/NUnitSample1Tests/UnitTests.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+using NUnit.Framework;
+
+namespace NUnitSample1
+{
+#region Test Code
+    [TestFixture]
+    public class AccountTest
+    {
+        Account source;
+        Account destination;
+
+        [SetUp]
+        public void Init()
+        {
+            source = new Account();
+            source.Deposit(200m);
+
+            destination = new Account();
+            destination.Deposit(150m);
+        }
+
+        [Test]
+        public void TransferFunds()
+        {
+            source.TransferFunds(destination, 100m);
+
+            Assert.AreEqual(250m, destination.Balance);
+            Assert.AreEqual(100m, source.Balance);
+        }
+
+        [Test]
+        [Ignore ("Decide how to hande transactoins.")]
+        [ExpectedException(typeof(InsufficientFundsException))]
+        public void TransferWithInsufficientFunds()
+        {
+            source.TransferFunds(destination, 300m);
+        }
+
+        [Test]
+        public void TransferWithInsufficientFundsAtomicity()
+        {
+            try
+            {
+                source.TransferFunds(destination, 300m);
+            }
+            catch (InsufficientFundsException expected)
+            {
+            }
+
+            Assert.AreEqual(200m, source.Balance);
+            Assert.AreEqual(150m, destination.Balance);
+        }
+    }
+#endregion
+}
---
+++ NUnitSamples/NUnitSample1/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+#region Program code
+namespace NUnitSample1
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("End of Program");
+        }
+    }
+
+    public class Account
+    {
+        private decimal balance;
+        public decimal Balance
+        {
+            get { return balance; }
+        }
+
+        private decimal minimumBalance = 10m;
+        public decimal MinimimumBalance
+        {
+            get { return minimumBalance; }
+        }
+
+        public void Deposit(decimal amount)
+        {
+            balance += amount;
+        }
+
+        public void Withdraw(decimal amount)
+        {
+            balance -= amount;
+        }
+
+        public void TransferFunds(Account destination, decimal amount)
+        {
+            if (balance - amount < minimumBalance)
+                throw new InsufficientFundsException();
+
+            destination.Deposit(amount);
+            Withdraw(amount);
+        }
+    }
+
+    public class InsufficientFundsException : ApplicationException
+    {
+    }
+}
+#endregion
+
